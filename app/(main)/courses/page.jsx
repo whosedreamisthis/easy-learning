14ba73865889bb2c8e7e7e49c7f27b5e73d0a7cc
Dana Sharon <whosedreamisthis@gmail.com>
2025-07-22 12:54:14 -0700

    courses page shows dynamic courses, remoced hardcoded courses

--- app/(main)/courses/page.jsx
+++ app/(main)/courses/page.jsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { Button } from '@/components/ui/button';
 
 import { formatPrice } from '@/lib/formatPrice';
@@ -8,58 +6,60 @@ import { ArrowRight } from 'lucide-react';
 import { BookOpen } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useState } from 'react';
 import SearchCourse from './_components/search-course';
 
 import FilterCourseMobile from './_components/filter-course-mobile';
 import ActiveFilters from './_components/active-filters';
 import FilterCourse from './_components/filter-course';
+import { getCourseList } from '@/queries/courses';
+import CourseCard from './_components/course-card';
 
-const courses = [
-	{
-		id: 1,
-		title: 'Design',
-		thumbnail: '/assets/images/categories/design.jpg',
-	},
+// const courses = [
+// 	{
+// 		id: 1,
+// 		title: 'Design',
+// 		thumbnail: '/assets/images/categories/design.jpg',
+// 	},
 
-	{
-		id: 3,
-		title: 'Development',
-		thumbnail: '/assets/images/categories/development.jpg',
-	},
-	{
-		id: 4,
-		title: 'Marketing',
-		thumbnail: '/assets/images/categories/marketing.jpg',
-	},
-	{
-		id: 5,
-		title: 'IT & Software',
-		thumbnail: '/assets/images/categories/it_software.jpg',
-	},
-	{
-		id: 6,
-		title: 'Personal Development',
-		thumbnail: '/assets/images/categories/personal_development.jpg',
-	},
-	{
-		id: 7,
-		title: 'Business',
-		thumbnail: '/assets/images/categories/business.jpg',
-	},
-	{
-		id: 8,
-		title: 'Photography',
-		thumbnail: '/assets/images/categories/photography.jpg',
-	},
-	{
-		id: 9,
-		title: 'Music',
-		thumbnail: '/assets/images/categories/music.jpg',
-	},
-];
+// 	{
+// 		id: 3,
+// 		title: 'Development',
+// 		thumbnail: '/assets/images/categories/development.jpg',
+// 	},
+// 	{
+// 		id: 4,
+// 		title: 'Marketing',
+// 		thumbnail: '/assets/images/categories/marketing.jpg',
+// 	},
+// 	{
+// 		id: 5,
+// 		title: 'IT & Software',
+// 		thumbnail: '/assets/images/categories/it_software.jpg',
+// 	},
+// 	{
+// 		id: 6,
+// 		title: 'Personal Development',
+// 		thumbnail: '/assets/images/categories/personal_development.jpg',
+// 	},
+// 	{
+// 		id: 7,
+// 		title: 'Business',
+// 		thumbnail: '/assets/images/categories/business.jpg',
+// 	},
+// 	{
+// 		id: 8,
+// 		title: 'Photography',
+// 		thumbnail: '/assets/images/categories/photography.jpg',
+// 	},
+// 	{
+// 		id: 9,
+// 		title: 'Music',
+// 		thumbnail: '/assets/images/categories/music.jpg',
+// 	},
+// ];
 
-const CoursesPage = () => {
+const CoursesPage = async () => {
+	const courses = await getCourseList();
 	return (
 		<section
 			id="courses"
@@ -88,61 +88,9 @@ const CoursesPage = () => {
 					<FilterCourse />
 					{/* Course grid */}
 					<div className="lg:col-span-3 grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4">
-						{courses.map((category) => {
+						{courses.map((course) => {
 							return (
-								<Link
-									key={category.id}
-									href={`/courses/${category.id}`}
-								>
-									<div className="group hover:shadow-sm transition overflow-hidden border rounded-lg p-3 h-full">
-										<div className="relative w-full aspect-video rounded-md overflow-hidden">
-											<Image
-												src="/assets/images/courses/course_1.png"
-												alt={'course'}
-												className="object-cover"
-												fill
-											/>
-										</div>
-										<div className="flex flex-col pt-2">
-											<div className="text-lg md:text-base font-medium group-hover:text-sky-700 line-clamp-2">
-												Reactive Accelerator
-											</div>
-											<p className="text-xs text-muted-foreground">
-												Development
-											</p>
-											<div className="my-3 flex items-center gap-x-2 text-sm md:text-xs">
-												<div className="flex items-center gap-x-1 text-slate-500">
-													<div>
-														<BookOpen className="w-4" />
-													</div>
-													<span>4 Chapters</span>
-												</div>
-											</div>
-
-											{/* <CourseProgress
-												size="sm"
-												value={80}
-												variant={
-													110 === 100 ? 'success' : ''
-												}
-											/> */}
-
-											<div className="flex items-center justify-between mt-4">
-												<p className="text-md md:text-sm font-medium text-slate-700">
-													{formatPrice(49)}
-												</p>
-
-												<Button
-													variant="ghost"
-													className="text-xs text-sky-700 h-7 gap-1"
-												>
-													Enroll
-													<ArrowRight className="w-3" />
-												</Button>
-											</div>
-										</div>
-									</div>
-								</Link>
+								<CourseCard key={course.id} course={course} />
 							);
 						})}
 					</div>
